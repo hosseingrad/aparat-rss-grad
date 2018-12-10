f06226273986c01d678dbb7edf63ba348d35120a
hosseingrad <hossein.g.rad@gmail.com>
2018-12-10 13:11:51 +0330

    change license version

--- wp-aparat-grad.php
+++ wp-aparat-grad.php
@@ -2,25 +2,25 @@
 /**
 * Plugin Name:  Aparat Videos RSS Reader | GRAD
 * Description:  Reading Aparat.com RSS and showing in a widget.
-* Version:      1.0.0
+* Version:      0.9.0
 * Author:       Hossein G Rad
 * Author URI:   https://www.linkedin.com/in/hosseingrad/
-* License:      GPL2
+* License:      GPL v3
 * Text Domain:  aparss-grad
 * Domain Path:  /languages
 *
-* Short News is free software: you can redistribute it and/or modify
+* This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* any later version.
-
-* Short News is distributed in the hope that it will be useful,
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-
+*
 * You should have received a copy of the GNU General Public License
-* along with Short News. If not, see {License URI}.
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 */
 
